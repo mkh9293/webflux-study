52c9c99550a44ff816c3c4a046e09a6888cf0dee
mkh9293 <mkh9293@gmail.com>
2021-10-10 00:49:10 +0900

    crud data with functional endpoint

---
+++ src/main/java/com/learnreactivespring/handler/ItemHandler.java
@@ -0,0 +1,83 @@
+package com.learnreactivespring.handler;
+
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
+@Component
+public class ItemHandler {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    static Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+
+    public Mono<ServerResponse> getAllItems(ServerRequest serverRequest) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemReactiveRepository.findAll(), Item.class);
+    }
+
+    public Mono<ServerResponse> getOneItem(ServerRequest serverRequest) {
+
+        String id = serverRequest.pathVariable("id");
+        Mono<Item> itemMono = itemReactiveRepository.findById(id);
+
+        return itemMono.flatMap(item ->
+                ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(item)))
+                .switchIfEmpty(notFound);
+    }
+
+    public Mono<ServerResponse> createItem(ServerRequest serverRequest) {
+
+        Mono<Item> itemTobeInserted = serverRequest.bodyToMono(Item.class);
+
+        return itemTobeInserted.flatMap(item ->
+                ServerResponse.status(HttpStatus.CREATED)
+                        .contentType(MediaType.APPLICATION_JSON)
+                .body(itemReactiveRepository.save(item), Item.class)
+        );
+
+    }
+
+    public Mono<ServerResponse> deleteItem(ServerRequest serverRequest) {
+
+        String id = serverRequest.pathVariable("id");
+
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemReactiveRepository.deleteById(id), Void.class);
+
+    }
+
+    public Mono<ServerResponse> updateItem(ServerRequest serverRequest) {
+
+        String id = serverRequest.pathVariable("id");
+        Mono<Item> updatedItem = serverRequest.bodyToMono(Item.class)
+                .flatMap(item ->
+                        itemReactiveRepository.findById(id)
+                                .flatMap(curItem -> {
+                                    curItem.setDescription(item.getDescription());
+                                    curItem.setPrice(item.getPrice());
+                                    return itemReactiveRepository.save(curItem);
+                                })
+                );
+
+        return updatedItem.flatMap(item ->
+                ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(item)))
+                .switchIfEmpty(notFound);
+
+    }
+}
---
+++ src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,138 @@
+package com.learnreactivespring.handler;
+
+import com.learnreactivespring.constants.v1.ItemConstants;
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.learnreactivespring.constants.v1.ItemConstants.ITEM_END_POINT_V1;
+import static com.learnreactivespring.constants.v1.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item("ABC", "Beats Headphones", 19.99)
+        );
+    }
+
+    @BeforeEach
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println("Inserted item is : " + item))
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void getOneItem() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "ABC")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.price", 19.99);
+    }
+
+    @Test
+    public void getOneItem_notFound() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "DEF")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+
+    @Test
+    public void createItem() {
+        Item item = new Item(null, "Iphone X", 999.99);
+
+        webTestClient.post().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("Iphone X")
+                .jsonPath("$.price").isEqualTo(999.99);
+    }
+
+    @Test
+    public void deleteItem() {
+
+        webTestClient.delete().uri(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "ABC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Void.class);
+    }
+
+    @Test
+    public void updateItem() {
+        double newPrice = 129.99;
+
+        Item item = new Item(null, "Beats Headphones", newPrice);
+
+        webTestClient.put().uri(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "ABC")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.price", 129.99);
+    }
+
+    @Test
+    public void updateItem_notFound() {
+        double newPrice = 129.99;
+
+        Item item = new Item(null, "Beats Headphones", newPrice);
+
+        webTestClient.put().uri(ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "DEF")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+}
