2d147abb14eb82c328a4b0d6940063a33fa5b11e
mkh9293 <mkh9293@gmail.com>
2021-12-30 23:00:56 +0900

    retry fail http call

--- movies-service/src/main/java/com/reactivespring/exception/ReviewsServerException.java
+++ movies-service/src/main/java/com/reactivespring/exception/ReviewsServerException.java
@@ -4,6 +4,7 @@ public class ReviewsServerException extends RuntimeException {
     private String message;
 
     public ReviewsServerException(String message) {
+        super(message);
         this.message = message;
     }
 }
--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -1,5 +1,6 @@
 package com.reactivespring.controller;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.reactivespring.domain.Movie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -126,6 +127,7 @@ public class MoviesControllerIntgTest {
                 .expectBody(String.class)
                 .isEqualTo("There is no Movie avaliable for the passed in Id : abc");
 
+        WireMock.verify(1, getRequestedFor(urlEqualTo("/v1/movieinfos" + "/" + movieId)));
     }
 
     @Test
@@ -189,5 +191,43 @@ public class MoviesControllerIntgTest {
                 .expectBody(String.class)
                 .isEqualTo("Server Exception in MoviesInfoService MovieInfo Service Unavaliable");
 
+        WireMock.verify(4, getRequestedFor(urlEqualTo("/v1/movieinfos" + "/" + movieId)));
+    }
+
+    @Test
+    void retrieveMovieById_reviews_5xx() {
+        var movieId = "abc";
+
+        stubFor(
+                get(urlEqualTo("/v1/movieinfos" + "/" + movieId))
+                        .willReturn(aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withBody("{\n" +
+                                        "  \"movieInfoId\": \"1\",\n" +
+                                        "  \"name\": \"Batman Begins\",\n" +
+                                        "  \"year\": 2005,\n" +
+                                        "  \"cast\": [\n" +
+                                        "    \"Christian Bale\",\n" +
+                                        "    \"Michael Cane\"\n" +
+                                        "  ],\n" +
+                                        "  \"release_date\": \"2005-06-15\"\n" +
+                                        "}\n")
+                        )
+        );
+
+        stubFor(
+                get(urlPathEqualTo("/v1/reviews"))
+                        .willReturn(aResponse().withStatus(500).withBody("Review Service Not Avaliable"))
+        );
+
+        webTestClient
+                .get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectBody(String.class)
+                .isEqualTo("Server Exception in ReviewsService Review Service Not Avaliable");
+
+        WireMock.verify(4, getRequestedFor(urlPathEqualTo("/v1/reviews")));
     }
 }
